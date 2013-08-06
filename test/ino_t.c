bb312d489aac83383d9b59916cb63d94ff4e35a8
Robert Millan <rmh@debian.org>
2013-08-06 11:41:44 +0000

    Prevent Glibc ino_t from messing with kernel definition of ino_t (from now on known as '__kernel_ino_t').
    git-svn-id: svn://anonscm.debian.org/glibc-bsd/trunk/kfreebsd-kernel-headers@4871 ae44cbe4-c7d5-0310-ae45-95c72a56cd7d

---
+++ test/ino_t.c
@@ -0,0 +1,20 @@
+#define _FILE_OFFSET_BITS 64
+
+#include <sys/kern/time.h>
+#include <sys/kern/types.h>
+
+#include <ufs/ufs/dinode.h>
+#include <ufs/ffs/fs.h>
+
+#include <ufs/ufs/quota.h>
+#include <ufs/ufs/inode.h>
+
+#define ASSERT_SIZE(N, STRUCT, MEMBER, SIZE) \
+	static char __assert_ ## N [(sizeof (((struct STRUCT *) NULL)->MEMBER) == SIZE) ? 0 : -1]
+
+ASSERT_SIZE(1, fs, fs_snapinum[0], 4);
+ASSERT_SIZE(2, jrefrec, jr_ino, 4);
+ASSERT_SIZE(3, jrefrec, jr_parent, 4);
+ASSERT_SIZE(4, jmvrec, jm_ino, 4);
+ASSERT_SIZE(5, jmvrec, jm_parent, 4);
+ASSERT_SIZE(6, inode, i_number, 4);
