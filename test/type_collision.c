e319256b03f7bb7b4695885e23a36d5b108d124d
Robert Millan <rmh@debian.org>
2013-09-28 22:46:23 +0000

    test/type_collision.c: Include all types from glibc that may potentially conflict with <sys/kern/types.h>.
    git-svn-id: svn://anonscm.debian.org/glibc-bsd/trunk/kfreebsd-kernel-headers@4976 ae44cbe4-c7d5-0310-ae45-95c72a56cd7d

--- test/type_collision.c
+++ test/type_collision.c
@@ -1,2 +1,14 @@
 #include <sys/types.h>
+
+#define __need_clock_t
+#define __need_time_t
+#define __need_timer_t
+#include <time.h>
+
+/* mqd_t */
+#include <mqueue.h>
+
+#define __need_mbstate_t
+#include <wchar.h>
+
 #include <sys/kern/types.h>
