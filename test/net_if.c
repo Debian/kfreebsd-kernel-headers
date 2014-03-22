4fe1f6abc020f7a8d58115c2c467dd0234feb7fa
Robert Millan <rmh@debian.org>
2014-03-22 19:26:24 +0000

    Adjust testsuite to libc0.1-dev >= 2.18-2
    git-svn-id: svn://anonscm.debian.org/glibc-bsd/trunk/kfreebsd-kernel-headers@5452 ae44cbe4-c7d5-0310-ae45-95c72a56cd7d

--- test/net_if.c
+++ test/net_if.c
@@ -1,11 +1,3 @@
-#define ifaliasreq	gnu_ifaliasreq
-#define ifmediareq	gnu_ifmediareq
-#define ifdrv		gnu_ifdrv
 #include <net/if.h>
-#undef ifaliasreq
-#undef ifmediareq
-#undef ifdrv
-
-#include <sys/kglue/net/if.h>
 #include "assert.h"
 COMPILE_TIME_ASSERT(ifaliasreq_size, sizeof(struct ifaliasreq) == sizeof(struct oifaliasreq) + sizeof(int));
